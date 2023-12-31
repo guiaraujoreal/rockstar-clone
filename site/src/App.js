9f5fcfab331f87012d96936de792e5ef820bfc14
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-12-31 18:29:06 -0300

    inicio do projeto

---
+++ site/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Header';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
